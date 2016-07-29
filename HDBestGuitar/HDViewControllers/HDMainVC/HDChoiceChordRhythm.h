4ac1e9954a982574afdc8ef1ce28b5ba731cacf4
denglibing <13049862397@163.com>
2016-07-29 20:32:06 +0800

    添加选择和弦和节奏类

---
+++ HDBestGuitar/HDViewControllers/HDMainVC/HDChoiceChordRhythm.h
@@ -0,0 +1,13 @@
+//
+//  HDChoiceChordRhythm.h
+//  HDBestGuitar
+//
+//  Created by Harry on 16/7/29.
+//  Copyright © 2016年 HarryDeng. All rights reserved.
+//
+
+#import <HDBaseProject/HDBaseProject.h>
+
+@interface HDChoiceChordRhythm : HDBaseViewController
+
+@end
---
+++ HDBestGuitar/HDViewControllers/HDMainVC/HDChoiceChordRhythm.m
@@ -0,0 +1,19 @@
+//
+//  HDChoiceChordRhythm.m
+//  HDBestGuitar
+//
+//  Created by Harry on 16/7/29.
+//  Copyright © 2016年 HarryDeng. All rights reserved.
+//
+
+#import "HDChoiceChordRhythm.h"
+
+@implementation HDChoiceChordRhythm
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    
+    self.title = @"设置";
+}
+
+@end
